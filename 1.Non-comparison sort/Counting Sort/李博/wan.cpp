22d66568ee44d9ef419bc9cdf9ceaa97ce588103
chenxi222 <35353473+chenxi222@users.noreply.github.com>
2018-01-13 19:57:36 +0800

    123

---
+++ 1.Non-comparison sort/Counting Sort/李博/wan.cpp
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{ 
+/*	int i,j,s,k;
+	for(i=6;i<1000;i++)
+	{
+		j=i;
+		s=0;
+		for(k=1;k<j;k++)
+		{
+			if(j%k==0)
+			{
+				s=s+k;
+			}	
+		}
+		if(i==s)
+		{
+			printf("%d=1",i);
+			for(k=2;k<i;k++)
+			 {
+			 	if(i%k==0)
+			  		printf("+%d",k);
+			  }
+			  printf("\n");
+		}
+
+	}*/
+	char name[20]="asdfg";
+	printf("%d\n",sizeof(name));
+	printf("%d",strlen(name));
+	for(i=0;i<10;i++)
+	{
+		if(s[i].name[strlen(name)-1]=='z')
+		{
+			printf("%c",s[i].name);
+		}
+	}
+	return 0;
+ } 
